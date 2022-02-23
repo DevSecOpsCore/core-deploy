9b3b25f35f1da7c65cd5a08aae549dae663a317d
enespekdas <enes.pekdas@gmail.com>
2022-02-23 11:17:55 +0300

    deploy kubeconfig test v1.0.0.5.7.0

--- Library/Classes/deploy.py
+++ Library/Classes/deploy.py
@@ -11,19 +11,10 @@ class deployImageCluster:
     def setKubeConfigFile():
         mkdirKubeDirectory = subprocess.check_output("mkdir /home/runner/.kube ", shell=True)
         print(mkdirKubeDirectory)
-        catKubeconfig = subprocess.check_output("cat "+parseDeployData.commandParameters['kubeConfig'] + ' > /home/runner/.kube/config', shell=True)
-        print(catKubeconfig)
-        lsConfig=subprocess.check_output("ls -A /home/runner/.kube/ ", shell=True)
-        print(lsConfig)
-        printConfig = subprocess.check_output("echo config yazdiriyorum ", shell=True)
-        print(printConfig)
-        configControl = subprocess.check_output("cat /home/runner/.kube/config ", shell=True)
-        print(configControl)
-        kubectlControl = subprocess.check_output("kubectl get nodes ", shell=True)
-        print(kubectlControl)
-        print(parseDeployData.commandParameters)
-
-
+        lsyap = subprocess.check_output("echo ls yapıyorum   ", shell=True)
+        print(lsyap)
+        lsA = subprocess.check_output("ls -A /home/runner/ ", shell=True)
+        print(lsA)
   # - name: mkdir .kube directory
   #     run : mkdir /home/runner/.kube
   #   - name : echo secret
@@ -33,3 +24,18 @@ class deployImageCluster:
   #   - name: kubectl control
   #     run : kubectl get nodes
 
+
+
+#         mkdirKubeDirectory = subprocess.check_output("mkdir /home/runner/.kube ", shell=True)
+#         print(mkdirKubeDirectory)
+#         catKubeconfig = subprocess.check_output("cat "+parseDeployData.commandParameters['kubeConfig'] + ' > /home/runner/.kube/config', shell=True)
+#         print(catKubeconfig)
+#         lsConfig = subprocess.check_output("ls -A /home/runner/.kube/ ", shell=True)
+#         print(lsConfig)
+#         printConfig = subprocess.check_output("echo config yazdiriyorum ", shell=True)
+#         print(printConfig)
+#         configControl = subprocess.check_output("cat /home/runner/.kube/config ", shell=True)
+#         print(configControl)
+#         kubectlControl = subprocess.check_output("kubectl get nodes ", shell=True)
+#         print(kubectlControl)
+#         print(parseDeployData.commandParameters)
