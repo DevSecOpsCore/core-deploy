b52ae179fb714e97ecb9341f8fcd5256b8ad1fb8
enespekdas <enes.pekdas@gmail.com>
2022-02-23 11:04:49 +0300

    deploy kubeconfig test v1.0.0.5.5

--- Library/Classes/deploy.py
+++ Library/Classes/deploy.py
@@ -9,9 +9,21 @@ class deployImageCluster:
 
     @staticmethod
     def setKubeConfigFile():
-        echoCommand = subprocess.check_output("echo sa ", shell=True)
-        print(echoCommand)
+        mkdirKubeDirectory = subprocess.check_output("mkdir /home/runner/.kube ", shell=True)
+        print(mkdirKubeDirectory)
+        catKubeconfig = subprocess.check_output("cat "+parseDeployData.commandParameters['kubeConfig'] + ' > /home/runner/.kube/config', shell=True)
+        print(catKubeconfig)
+        configControl = subprocess.check_output("cat /home/runner/.kube/config ", shell=True)
+        print(configControl)
         print(parseDeployData.commandParameters)
 
 
+  # - name: mkdir .kube directory
+  #     run : mkdir /home/runner/.kube
+  #   - name : echo secret
+  #     run :  cat <<< "${{ secrets.KUBECONFIG }}" > /home/runner/.kube/config
+  #   - name : echo secret
+  #     run : echo   "${{ secrets.KUBECONFIG }}"
+  #   - name: kubectl control
+  #     run : kubectl get nodes
 
