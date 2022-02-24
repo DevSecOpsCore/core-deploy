45d00f95ea57cce755183eebb3ab7ce673f65a16
enespekdas <enes.pekdas@gmail.com>
2022-02-24 15:56:12 +0300

    deployment yaml to json v1.0.1.3.2.5

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-import json
-import os
-import sys
 import yaml
 
-from Library.Classes.deploy import deployImageCluster
+import yaml
 
-with open('test.yaml') as f:
-    dataMap = yaml.safe_load(f)
-
-    print(dataMap)
-print(" sa json olarak okuyom")
-
-print(deployImageCluster.readJsonFile)
+with open('temp.yaml') as f:
+    temp = yaml.safe_load_all(f)
+    print(temp)
